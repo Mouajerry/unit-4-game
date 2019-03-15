839249e44f98eb21aabc582a68b93e8576e1105b
Jerry Moua <jerrymoua@gmail.com>
2019-03-14 21:52:48 -0500

    adding character

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    var garlt;
+    var ciri;
+    var yennefer;
+    var drowner;
+    var leshen;
+    var noon;
+
+    var characterSelection = [];
+    var character = null;
+    var defenders = [];
+    var defender = null;
+    
+})
